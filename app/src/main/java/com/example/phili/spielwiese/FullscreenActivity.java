d0d0e0bd996f70c5acf232d79465c26fe13f0b54
Hoop77 <p.badenhoop@gmx.de>
2016-10-31 08:58:31 +0100

    big round textviews

---
+++ app/src/main/java/com/example/phili/spielwiese/FullscreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.phili.spielwiese;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fullscreen);
+    }
+}
